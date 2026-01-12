523f07ce6874dc47fecac90c27bc6e4a6dfd1feb
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-12 22:59:39 +0530

    Added solution for exercise 1.13(anecdotes step-1)

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function App() {
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+    "Programming is thinking, not typing.",
+    "The best way to get a project done faster is to start sooner.",
+    "First, solve the problem. Then, write the code.",
+    "If debugging is the process of removing bugs, then programming must be the process of putting them in.",
+    "Weeks of coding can save you hours of planning.",
+    "Software and cathedrals are much the same — first we build them, then we pray.",
+    "It works on my machine.",
+    "Premature optimization is the root of all evil.",
+  ];
+
+  const ranNum = () => {
+    let ranNum = Math.floor(Math.random() * (anecdotes.length - 1));
+    setSelected(ranNum);
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <div>
+        {" "}
+        <button onClick={ranNum}>Next Anectdote</button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
