d802d4ef425074b5fd2eb79c94fc7ce419bf0cd3
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-12 14:08:49 +0530

    Actual solution for excercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,24 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>Code here</div>;
+  const incGood = () => setGood(good + 1);
+  const incNeutral = () => setNeutral(neutral + 1);
+  const incBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={incGood}>good</button>
+        <button onClick={incNeutral}>neutral</button>
+        <button onClick={incBad}>bad</button>
+      </div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
 }
 
 export default App;
