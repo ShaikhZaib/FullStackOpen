d6d509dcbeed0d0e95e2dbd1efe702152f267c18
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-12 14:27:36 +0530

    Added solution for exercise 1.8(Unicafe Step-3)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,9 @@
 import { useState } from "react";
 
-function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const incGood = () => setGood(good + 1);
-  const incNeutral = () => setNeutral(neutral + 1);
-  const incBad = () => setBad(bad + 1);
-
-  const all = good + neutral + bad;
-  const avg = (good * 1 + neutral * 0 + bad * -1) / all;
-  const pos = (good / all) * 100;
-
+const Statistics = (props) => {
+  const { good, neutral, bad, all, avg, pos } = props;
   return (
     <div>
-      <div>
-        <h1>Give Feedback</h1>
-        <button onClick={incGood}>good</button>
-        <button onClick={incNeutral}>neutral</button>
-        <button onClick={incBad}>bad</button>
-      </div>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -30,6 +13,39 @@ function App() {
       <p>Positive: {pos} %</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incGood = () => setGood(good + 1);
+  const incNeutral = () => setNeutral(neutral + 1);
+  const incBad = () => setBad(bad + 1);
+
+  const all = good + neutral + bad;
+  const avg = (good * 1 + neutral * 0 + bad * -1) / all || 0;
+  const pos = (good / all) * 100 || 0;
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={incGood}>good</button>
+        <button onClick={incNeutral}>neutral</button>
+        <button onClick={incBad}>bad</button>
+      </div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        avg={avg}
+        pos={pos}
+      />
+    </div>
+  );
+};
 
 export default App;
