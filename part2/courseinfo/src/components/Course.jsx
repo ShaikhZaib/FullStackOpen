e1b3769bb0266ea00ee616a06091778dc093c8a0
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-13 15:24:15 +0530

    Added solution for exercise 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,17 @@
+const Course = ({ course }) => {
+  console.log(course);
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => {
+        return (
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Course;
