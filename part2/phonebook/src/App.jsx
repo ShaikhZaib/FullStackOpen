33edefb28b1e0792c7770838392b82de3c419a36
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-14 19:17:53 +0530

    Added solution for exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Zaib Shaikh" }]);
+  const [persons, setPersons] = useState([{}]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -11,14 +12,20 @@ function App() {
     }
     const NewPerson = {
       name: newName,
+      number: newNumber,
     };
 
     setPersons(persons.concat(NewPerson));
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -29,13 +36,18 @@ function App() {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <div key={person.name}>{person.name}</div>
+        {persons.map((person, index) => (
+          <div key={index}>
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
