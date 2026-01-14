52ca3ab93c9d0fe50632d4841be59782048c38d6
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-14 19:44:08 +0530

    Added solution for exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 function App() {
-  const [persons, setPersons] = useState([{}]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -13,6 +19,7 @@ function App() {
     const NewPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
     setPersons(persons.concat(NewPerson));
@@ -28,9 +35,31 @@ function App() {
     setNewNumber(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  // const personsToShow = (searchName) => {
+  //   if (searchName === "") return persons;
+  //   return persons.filter((person) =>
+  //     person.name.toLowerCase().includes(searchName.toLowerCase())
+  //   );
+  // };
+
+  const personsToShow =
+    searchName === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(searchName.toLowerCase())
+        );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input value={searchName} onChange={handleSearch} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,8 +73,8 @@ function App() {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person, index) => (
-          <div key={index}>
+        {personsToShow.map((person) => (
+          <div key={person.id}>
             {person.name} {person.number}
           </div>
         ))}
