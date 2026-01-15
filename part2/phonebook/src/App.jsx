da8efd78e7ba853f5277d943e36b880bccef3aab
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-15 12:27:26 +0530

    Added solution for exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/FIlter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchName, setSearchName] = useState("");
@@ -42,12 +38,14 @@ function App() {
     setSearchName(event.target.value);
   };
 
-  // const personsToShow = (searchName) => {
-  //   if (searchName === "") return persons;
-  //   return persons.filter((person) =>
-  //     person.name.toLowerCase().includes(searchName.toLowerCase())
-  //   );
-  // };
+  const getPersonsFromServer = () => {
+    axios.get("http://localhost:3001/persons").then((resonse) => {
+      console.log("Promise Fullfilled");
+      setPersons(resonse.data);
+    });
+  };
+
+  useEffect(getPersonsFromServer, []);
 
   const personsToShow =
     searchName === ""
