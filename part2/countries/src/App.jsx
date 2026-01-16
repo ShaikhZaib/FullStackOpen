047a483a12d68395bbacb33ce5b5bec7824ada6b
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-16 21:31:32 +0530

    Added solution for exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,29 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const api_key = import.meta.env.VITE_SOME_KEY;
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
+
 function App() {
   const [countries, setCountries] = useState([]);
+  const [weather, setWeather] = useState(null);
   const [searchCountryByName, setSearchCountryByName] = useState("");
-
-  const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 
   const handleCountrySearch = (event) => {
     setSearchCountryByName(event.target.value);
   };
-
-  useEffect(() => {
-    axios.get(`${baseUrl}/all`).then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
 
   const filteredCountries = countries.filter((country) =>
     country.name.common
       .toLowerCase()
       .includes(searchCountryByName.toLowerCase())
   );
+  const capital =
+    filteredCountries.length === 1 ? filteredCountries[0].capital[0] : null;
 
-  const country = filteredCountries[0];
+  useEffect(() => {
+    axios.get(`${baseUrl}/all`).then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+  useEffect(() => {
+    if (!capital) {
+      setWeather(null);
+      return;
+    }
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+      )
+      .then((response) => {
+        setWeather(response.data);
+      });
+  }, [capital]);
 
   return (
     <div>
@@ -49,25 +64,41 @@ function App() {
             ))}
           </ul>
         )}
-        {filteredCountries.length === 1 && (
-          <div>
-            <h1>{country.name.common}</h1>
-            <p>
-              {country.capital[0]} <br />
-              area: {country.area}
-            </p>{" "}
-            <h2>Languages</h2>
-            <ul>
-              {Object.values(country.languages).map((language) => (
-                <li key={language}>{language}</li>
-              ))}
-            </ul>
-            <img
-              src={country.flags.png}
-              alt={`flag of  ${country.name.common}`}
-            />
-          </div>
-        )}
+        {filteredCountries.length === 1 &&
+          (() => {
+            const country = filteredCountries[0];
+
+            return (
+              <div>
+                <h1>{country.name.common}</h1>
+                <p>
+                  Capital {country.capital[0]} <br />
+                  Area {country.area}
+                </p>
+                <h2>Languages</h2>
+                <ul>
+                  {Object.values(country.languages).map((language) => (
+                    <li key={language}>{language}</li>
+                  ))}
+                </ul>
+                <img
+                  src={country.flags.png}
+                  alt={`flag of ${country.name.common}`}
+                />
+                {weather && (
+                  <>
+                    <h2>Weather in {country.capital[0]}</h2>
+                    <p>Temperature {weather.main.temp} °C</p>
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt={weather.weather[0].description}
+                    />
+                    <p>Wind {weather.wind.speed} m/s</p>
+                  </>
+                )}
+              </div>
+            );
+          })()}
       </div>
     </div>
   );
