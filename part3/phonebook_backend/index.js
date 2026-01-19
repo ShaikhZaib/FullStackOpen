12240a591ac3444b557edbcefc9b1f01a7438c50
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-19 21:51:12 +0530

    Added the frontend to the backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("body", (request) => JSON.stringify(request.body));
 app.use(
