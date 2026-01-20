533e22caf26305e5350c243ac32a304f150f6d91
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-20 15:43:43 +0530

    Added solution for exercise 3.12

---
+++ part3/phonebook_backend/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
