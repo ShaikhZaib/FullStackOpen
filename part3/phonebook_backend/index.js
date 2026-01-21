e9606cfe12680e13bae66d9a0d2804290423fa55
ShaikhZaib <shaikhzaib04@gmail.com>
2026-01-21 22:39:01 +0530

    Added solution for exercise 3.19

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -46,14 +46,8 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "Required field is missng",
-    });
-  }
 
   // const nameExists = persons.find((person) => person.name === body.name);
 
@@ -68,17 +62,20 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, error) => {
-  const { number } = request.body;
+  const { name, number } = request.body;
 
   Person.findByIdAndUpdate(
     request.params.id,
-    { number },
+    { name, number },
     { new: true, runValidators: true, context: "query" },
   )
     .then((updatedPerson) => {
@@ -101,6 +98,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
